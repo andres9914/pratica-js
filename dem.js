283d48e66009c46dda994a0b95a5dac65742a70f
andres ruiz <eltangrox@gmail.com>
2020-10-29 22:54:34 -0500

    primera parte

---
+++ dem.js
@@ -0,0 +1,9 @@
+/*Colocar un JavaScript en un archivo externo tiene las siguientes ventajas:
+- Separa HTML y código.
+- Hace que HTML y JavaScript sean más fáciles de leer y mantener.
+- Los archivos JavaScript en caché pueden acelerar la carga de la página.*/
+alert("¡otro cuadro de alerta! :V");
+//LOOP'S
+for (i=1;i<=5;i++){
+    document.write(i+"<br>");
+}
